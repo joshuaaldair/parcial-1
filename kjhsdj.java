abc3568a29cf259d8171b8faf5e0ff7d5e6aa824
joshuaaldair <39605628+joshuaaldair@users.noreply.github.com>
2018-06-28 19:50:01 -0500

    problma 1, parcial 1

---
+++ kjhsdj.java
@@ -0,0 +1,19 @@
+package lahdlad;
+
+import java.util.Scanner;
+
+
+public class kjhsdj {
+
+	public static void main(String[] args) {
+	float a,b=(float) 0.21,c;
+	Scanner scan=new Scanner(System.in);
+	System.out.println("introduzca el precio del producto");
+	a=scan.nextFloat();
+	c=(a*b)+a;
+	System.out.println("el precio final del producto es:\n"+c);
+	
+
+	}
+
+}
